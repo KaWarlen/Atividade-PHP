0171c4dd2a6dec31f0d2fc7fac6cd75a22aef912
KaWarlen <kwarlllen@gmail.com>
2026-03-03 07:55:34 -0300

    --rebase da atividade de controllers para o repositorio

---
+++ controllers/atividade_controllers/app/Http/Controllers/CursoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function index() {
+    return "Lista de cursos";
+}
+public function create() {
+    return view('cursos.create');
+}
+
+public function listagem() {
+    $cursos = ['Laravel Pro', 'PHP Moderno', 'Vue.js Mastery'];
+    return view('cursos.listagem', compact('cursos'));
+}
+public function show($id) {
+    return "Curso selecionado: ID " . $id;
+}
+
+}
